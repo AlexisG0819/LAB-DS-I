934edd999a7e7b6312caf3dbca12068762f4411b
AlexisG0819 <alexxis.gutierrez.2017@gmail.com>
2023-09-09 09:25:05 -0600

    Guias Subidas

---
+++ GUIA 1/Notas/src/main/java/com/mycompany/notas/Notas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.notas;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Notas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
